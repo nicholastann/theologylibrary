943528ad8a855912ec32666bad8722b8649c5514
nicholastann <54993961+nicholastann@users.noreply.github.com>
2022-03-29 09:14:18 -0500

    init

--- index.php
+++
@@ -1,8 +0,0 @@
-<h1 style="color:white; text-align:center;">Placeholder Text</h1>
-<body style="background-color:#101020;">
-    <iframe width="100%" height="100%" frameborder="0" src="https://observablehq.com/embed/@nicholastann/zoomable-sunburst?cells=chart"></iframe>
-</body>
-
-
-
-<link rel="stylesheet" href="style.css">
